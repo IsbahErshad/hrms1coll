12cce77589b053968c3313c51b792bebc4c2265b
IsbahErshad <156588540+IsbahErshad@users.noreply.github.com>
2025-07-13 22:04:34 +0530

    db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Ac1oMy@M3',
+  password: '',
   database: 'HRManagementSystem',
 });
 
